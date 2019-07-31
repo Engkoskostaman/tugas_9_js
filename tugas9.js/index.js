a7c715ad84be85061914f3f73504baeaa6538ec5
Engkos <engkoskostaman97@gmail.com>
2019-08-01 02:13:24 +0700

    tugas 9 js
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas9.js/index.js
@@ -0,0 +1,12 @@
+function panggilobjects(){
+  var karyawan ={
+     nama     : "budi",
+     umur     : 25,
+     bagian   :"gudang",
+     lamakerja:"2 tahun"
+  }
+for(var x in karyawan)
+console.log(karyawan[x])
+}
+
+panggilobjects()
